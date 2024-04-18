8ba232cbe62c163d027f1b6a17a195f935494937
h4ll0w0rld <80950527+h4ll0w0rld@users.noreply.github.com>
2024-04-18 13:14:00 +0200

    why do i miss

--- Api/server.js
+++ Api/server.js
@@ -46,7 +46,7 @@ async function saveHumidityData(_id, _humidity, _time) {
   try {
     const query = 'INSERT INTO HumidSens (devId, humidity, Date) VALUES (?)';
     await db.execute(query, [_id, _humidity, _time]);
-    console.log(`Saved humidity value ${_humidity} to the database with id: ${_id} collected ${time.toString()}.`);
+    console.log(`Saved humidity value ${_humidity} to the database with id: ${_id} collected ${_time.toString()}.`);
   } catch (error) {
     console.error('Error saving humidity data:', error.message);
   }
