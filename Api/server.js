7e42ef58cc30e73add228889de6c1f2e4dc30713
h4ll0w0rld <80950527+h4ll0w0rld@users.noreply.github.com>
2024-05-08 16:47:19 +0200

    sql syntax

--- Api/server.js
+++ Api/server.js
@@ -36,7 +36,7 @@ app.post('/register', async (req, res) => {
   console.log(req.body); // Add this line to log the request body
   const hashedPassword = await bcrypt.hash(password, 10);
   try {
-      await db.query('INSERT INTO users (username, password) VALUES (?, ?, ?)', [username, hashedPassword]);
+      await db.query('INSERT INTO user (username, password) VALUES (?, ?)', [username, hashedPassword]);
       res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
       console.error(error);
