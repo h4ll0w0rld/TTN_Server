63f6643e3adc19e6f86329852567b88a74c1bed5
h4ll0w0rld <80950527+h4ll0w0rld@users.noreply.github.com>
2024-05-08 17:04:21 +0200

    more SQL joy

--- Api/server.js
+++ Api/server.js
@@ -37,8 +37,8 @@ app.post('/register', async (req, res) => {
   const hashedPassword = await bcrypt.hash(password, 10);
   
   try {
-    const connection = await pool.getConnection();
-    await connection.query('INSERT INTO user (username, password) VALUES (?, ?)', [username, hashedPassword]);
+   
+    await db.query('INSERT INTO user (username, password) VALUES (?, ?)', [username, hashedPassword]);
     connection.release();
     res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
