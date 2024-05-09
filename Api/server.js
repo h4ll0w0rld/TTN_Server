8f989831c2d817a996764bda4d7c0deb5f1df98b
h4ll0w0rld <80950527+h4ll0w0rld@users.noreply.github.com>
2024-05-09 13:43:28 +0200

    ablauf

--- Api/server.js
+++ Api/server.js
@@ -66,7 +66,7 @@ app.post('/login', async (req, res) => {
 
   //resolve(results);
 
-  user = getUserByName(username);
+  user = await getUserByName(username);
   if (!user) return res.status(401).json({ message: 'Invalid username or password' });
 
   try {
