51ae55462df8d6f0316682574959006dd8464703
h4ll0w0rld <80950527+h4ll0w0rld@users.noreply.github.com>
2024-05-22 15:36:06 +0200

    async

--- Api/controllers/auth-controller.js
+++ Api/controllers/auth-controller.js
@@ -3,13 +3,13 @@ const AuthService = require('../services/auth-service');
 const register = async (req, res) => {
     const { username, password } = req.body;
     try {
-      await AuthService.registerUser(username, password);
-      res.status(200).send('User created');
+        await AuthService.registerUser(username, password);
+        res.status(200).send('User created');
     } catch (error) {
-      console.error('Error registering user:', error);
-      res.status(400).send(error.message);
+        console.error('Error registering user:', error);
+        res.status(400).send(error.message);
     }
-  };
+};
 
 
 async function login(req, res) {
@@ -17,7 +17,8 @@ async function login(req, res) {
     const { username, password } = req.body;
     if (!AuthService.userExists()) return res.status(401).json({ message: 'Invalid username or password' });
     try {
-        const jwtToken = AuthService.loginUser(username, password)
+        const jwtToken = await AuthService.loginUser(username, password)
+        console.log(jwtToken)
         res.json(jwtToken)
     } catch (err) {
         console.log("Wrong credentials", err)
@@ -28,4 +29,4 @@ async function login(req, res) {
 module.exports = {
     register,
     login
-  };
+};
