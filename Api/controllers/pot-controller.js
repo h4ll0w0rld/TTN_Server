9d3c341005b656c34b4cbc56161540464f444a80
h4ll0w0rld <80950527+h4ll0w0rld@users.noreply.github.com>
2024-05-23 18:08:24 +0200

    a

--- Api/controllers/pot-controller.js
+++ Api/controllers/pot-controller.js
@@ -53,6 +53,7 @@ async function delPot(req, res) {
 async function delLog(req, res) {
     try {
         const { potId, logId } = req.body;
+        console.log("Dealing with: ", potId, logId)
         const response = await PotService.delLog(potId, logId);
         res.status(200).json("Log detatched", response);
     } catch (err) {
