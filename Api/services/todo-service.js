68762d3bd62353bad8391457ec1dd74639622379
h4ll0w0rld <80950527+h4ll0w0rld@users.noreply.github.com>
2024-05-24 14:42:38 +0200

    aa
    a

--- Api/services/todo-service.js
+++ Api/services/todo-service.js
@@ -54,10 +54,9 @@ function createAutoTask(){
                     if (sensor.value < pot.waterthreshhold) {
                         const title = `${pot.title} Braucht wasser`;
                         const description = `Der Wassergehalt von Topf nr ${pot.id} ist niedrig`;
-                        db.query('INSERT INTO ToDo (title, description) VALUES (?, ?)', [title, description], (error, results) => {
-                            if (error) throw error;
-                            console.log('ToDo task created:', results.insertId);
-                        });
+                        createTodo(title, description, false);
+
+                     
                     }
                 });
             });
