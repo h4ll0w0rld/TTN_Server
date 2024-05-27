f65d92e4dcf2fceca36194179f9a63a4f029c0cd
h4ll0w0rld <80950527+h4ll0w0rld@users.noreply.github.com>
2024-05-27 13:15:59 +0200

    a

--- Api/server.js
+++ Api/server.js
@@ -17,7 +17,7 @@ const storage = multer.diskStorage({
     cb(null, './uploads/');
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + process.env.BASE_URL.extname(file.originalname));
+    cb(null, file.originalname);       //Date.now() + process.env.BASE_URL.extname(
   }
 });
 
