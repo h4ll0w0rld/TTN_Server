e708779e78e492acf2bbc834618c069c599ebcb7
h4ll0w0rld <80950527+h4ll0w0rld@users.noreply.github.com>
2024-05-27 14:21:17 +0200

    gback

--- Api/routs/log-routes.js
+++ Api/routs/log-routes.js
@@ -11,7 +11,7 @@ const storage = multer.diskStorage({
     },
     filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, 'http://' + uniqueSuffix + path.extname(file.originalname));
+        cb(null, uniqueSuffix + path.extname(file.originalname));
     }
 });
 
