e63bf5fadab1ca24842b9850bc579d08d7980319
h4ll0w0rld <80950527+h4ll0w0rld@users.noreply.github.com>
2024-06-30 14:47:27 +0200

    Changes

--- Api/services/todo-service.js
+++ Api/services/todo-service.js
@@ -56,8 +56,8 @@ async function createAutoTask() {
             console.log("Checking Pot nr", pot)
             console.log("SENSOR: ", sensor)
             if (sensor.humidity < pot.autoWateringTodo) {
-                const headline = `${pot.id} Braucht wasser`;
-                const description = `Der Wassergehalt von Topf nr ${pot.name} ist niedrig`;
+                const headline = `${pot.name} braucht wasser`;
+                const description = `Der Wassergehalt von Topf ${pot.id} ist zu gering`;
 
                 const exists = await todoExists(headline);
                 console.log(exists)
